98eb74f934fec874fdd62579452b624738db5123
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-22 12:39:41 +0200

    refactor -v option

--- src/cli.ts
+++ src/cli.ts
@@ -6,13 +6,19 @@ export const cli = new Command()
         `A cli to watch build output folders and upload them to some servers via ssh`,
     )
     .version("0.1.0") //TODO: add auto-version via deno.json or smth. else
-    .option("-v, --pureVersion", "Outputs the pure version number", {
+    .option("-v, --pure-version", "Outputs the pure version number", {
+        // this option is standalone, so it does not execute the main action() handler of this cli, but it's own action
         standalone: true,
-    })
-    .action(({ pureVersion }) => {
-        // log the version when -v flag is given
-        if (pureVersion) {
+        action: () => {
             console.log(cli.getVersion());
-            return;
-        }
+        },
+    })
+    .option(
+        "-u, --upload-pair",
+        "An upload-pair in the format of <source>:<destination>. Note: this option can be set multiple times, but destination must be on the same host for each upload-pair",
+        {
+            collect: true,
+        },
+    )
+    .action(() => {
     });
