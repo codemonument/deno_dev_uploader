78d55de73677c942964bfd789e7c34521552c4e2
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-22 12:31:12 +0200

    add a pure-version command

--- src/cli.ts
+++ src/cli.ts
@@ -6,4 +6,12 @@ export const cli = new Command()
         `A cli to watch build output folders and upload them to some servers via ssh`,
     )
     .version("0.1.0") //TODO: add auto-version via deno.json or smth. else
-;
+    .option("-v, --pureVersion", "Outputs the pure version number", {
+        standalone: true,
+    })
+    .action(({ pureVersion }) => {
+        // log the version when -v flag is given
+        if (pureVersion) {
+            console.log(cli.getVersion());
+        }
+    });
