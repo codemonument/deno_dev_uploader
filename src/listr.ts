2f0682f84969ba3d9e7d22f19c9d32587d76555b
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-22 18:14:18 +0200

    add function to create listr tasks per upload list

--- src/listr.ts
+++ src/listr.ts
@@ -1,6 +1,14 @@
 import { Manager } from "@listr2/manager";
-import { type ListrBaseClassOptions, ListrLogger } from "npm:listr2";
+import {
+    type DefaultRenderer,
+    type ListrBaseClassOptions,
+    ListrLogger,
+    type ListrTaskWrapper,
+    type SimpleRenderer,
+} from "npm:listr2";
 import type { GenericLogger, SftpClient } from "@codemonument/sftp-client";
+import { finalize, map } from "rxjs";
+import { roundToPrecision } from "@codemonument/simple-rounding";
 
 export type ListrCtx = {
     sftp: Array<SftpClient>;
@@ -21,6 +29,41 @@ export function createListrManager<T = unknown>(
         },
         ...override,
     });
+}
+
+/**
+ * Creates a listr task for each SFTP Process per watcher emit
+ * @param sftp
+ * @param files
+ * @param uploaderName
+ * @returns
+ */
+function createSftpUploadTask(
+    sftp: SftpClient,
+    files: string[],
+    uploaderName: string,
+) {
+    return (
+        _ctx: ListrCtx,
+        task: ListrTaskWrapper<
+            any,
+            typeof DefaultRenderer,
+            typeof SimpleRenderer
+        >,
+    ) => {
+        const start = performance.now();
+        const fileList = files;
+        return sftp.uploadFiles$(fileList).pipe(
+            map(({ file, nr }) => `Uploading file ${nr}: ${file}`),
+            finalize(() => {
+                const end = performance.now();
+                const durationInSek = roundToPrecision((end - start) / 1000, 2);
+                const text =
+                    `${uploaderName}: Uploaded ${fileList.length} files in ${durationInSek} seconds!`;
+                task.title = text;
+            }),
+        );
+    };
 }
 
 const internalListrLogger = new ListrLogger({
