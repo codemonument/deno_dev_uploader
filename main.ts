833f3bf2c1ebe7bc7951131a841afc086734c4ef
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-22 12:15:42 +0200

    deno add jsr:@codemonument/cliffy-zod-option

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
+import { Command } from "@cliffy/command";
+
 // Will be executed when this module is run directly as an entrypoint
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Hello world");
+if (!import.meta.main) {
+  throw new Error("This module is a cli and can only be run directly!");
 }
+
+console.log("Hello world");
