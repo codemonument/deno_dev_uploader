b1ddc48e21ab9a2ee0e8944049f6ea5edc561fd1
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-22 11:54:10 +0200

    cleanup main.ts and tests

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # deno_dev_uploader
+
 A small cli to watch build output folders and upload them to some servers via ssh
+
+---
+
+# Changelog
+
+## 0.1.0 - WIP
+
+- initial release
--- main.ts
+++ main.ts
@@ -1,8 +1,5 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
+// Will be executed when this module is run directly as an entrypoint
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  console.log("Hello world");
 }
